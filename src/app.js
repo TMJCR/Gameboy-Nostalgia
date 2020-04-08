aa4169e66ad98c9c3716ccd86c178c0bd11de053
ThomasC <thomascrowdy@gmail.com>
2020-04-08 10:47:33 +0100

    Configuration changes for deployment

--- src/app.js
+++ src/app.js
@@ -2,12 +2,15 @@ const path = require('path');
 const express = require('express');
 
 const app = express();
+const port = process.env.PORT || 3000;
+
 const publicDirectoryPath = path.join(__dirname, '../public');
 app.use(express.static(publicDirectoryPath));
 
 app.get('/*', (req, res) => {
   res.redirect('/');
 });
-app.listen(3000, () => {
-  console.log('App is up and running on port 3000');
+
+app.listen(port, () => {
+  console.log(`App is up and running on port ${port}`);
 });
