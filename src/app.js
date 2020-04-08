9a2f39f6d83290dd79cfa4154576b01b1633ac19
ThomasC <thomascrowdy@gmail.com>
2020-04-08 11:04:58 +0100

    Sprite Added to folder

--- src/app.js
+++ src/app.js
@@ -8,6 +8,10 @@ const publicDirectoryPath = path.join(__dirname, '../public');
 app.use(express.static(publicDirectoryPath));
 
 app.get('/*', (req, res) => {
+  res.send('/index.html');
+});
+
+app.get('/*', (req, res) => {
   res.redirect('/');
 });
 
